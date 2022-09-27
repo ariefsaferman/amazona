761161756e5fa9e9576f9f5b975825cc83faabc5
ariefsaferman <arief.saferman@gmail.com>
2022-09-27 15:23:20 +0700

    feat: add data json object and render the object

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
+import data from "./data";
+
 function App() {
   return (
     <div>
       <header>
-        <a href="/">amazona</a>
+        <a href="/">2 + 2 != 5</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((products) => (
+            <div className="product" key={products.slug}>
+              <a href={`/product/${products.slug}`}>
+                <img src={products.image} alt={products.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${products.slug}`}>
+                  <p>{products.name}</p>
+                </a>
+                <p>{products.price}</p>
+                <button> Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
