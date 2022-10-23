2855135b95d6491d2418cf0bc72990c6fb30b775
ariefsaferman <arief.saferman@gmail.com>
2022-10-23 11:29:39 +0700

    feat: add placeOrderScreen

--- frontend/src/screens/ShippingAddressScreen.js
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -3,7 +3,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import { useNavigate } from "../../node_modules/react-router-dom/dist/index";
+import { useNavigate } from "react-router-dom";
 import { Store } from "../Store";
 import CheckoutSteps from "../components/CheckoutSteps";
 
